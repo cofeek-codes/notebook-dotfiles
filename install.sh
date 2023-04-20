452e9c059f71cd26e28585213ff4877be83f3676
cofeek-codes <kormyshev11@mail.ru>
2023-04-20 20:10:00 +0500

    fonts left

--- install.sh
+++ install.sh
@@ -1,3 +1,27 @@
 sudo pacman -S (cat paclist) --needed --noconfirm
 sudo systemctl enable ly
+
+# fish part
+fish
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher install ilancosman/tide@v5
+fisher install jethrokuan/z
+# fish part
+
 ./apply-config.sh
+
+
+# aliases
+alias --save ll "exa -l -g --icons"
+alias --save vim nvim
+
+# apply config
+cp */ ~/.config/ -r
+
+#fs
+mkdir -p ~/Desktop/codes/{python/, web/, mobile/, desktop/, main-projects/}
+# scripts
+mkdir -p ~/tools/scripts
+git clone https://github.com/cofeek-codes/dotfiles-scripts.git ~/tools/scripts
+exit
+
